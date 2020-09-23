0e5b97029f7dbcad586a7088737b94d4f3df9267
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-23 06:58:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,15 @@ class Artist
   
   attr_accessor :name 
   
+  @@song_count
+  
   def initialize(name)
     @name = name
   end 
   
   def songs 
     Song.all.select {|song| song.artist == self}
+    @@song_count += 1 
   end
   
   def add_song(song)
