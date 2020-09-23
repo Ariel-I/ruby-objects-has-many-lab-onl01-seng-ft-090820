dca2f4e0b6584da4d185ba42dacae1f13aff7445
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-23 07:00:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,21 +10,22 @@ class Artist
   
   def songs 
     Song.all.select {|song| song.artist == self}
-    @@song_count += 1 
   end
   
   def add_song(song)
     song.artist = self 
+    @@song_count += 1
   end 
   
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     add_song(song)
+    @@song_count += 1
   end 
   
   def self.song_count 
-    @@all = songs 
+    @@song_count 
   end 
   
 end 
