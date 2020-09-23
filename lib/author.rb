c7c0bf1dda197f3c5c3b6fc72d50e4a8dfccf203
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-23 06:31:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,26 @@ class Author
   
   attr_accessor :name, :post 
   
+  def initialize(name)
+    @name = name
+  end 
   
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self 
+  end 
+  
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end 
+  
+  def self.song_count 
+    @@all = songs 
+  end 
   
 end
