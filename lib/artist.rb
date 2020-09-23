2320b8598e0840c623cda8926f508c360e85f9fd
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-23 06:13:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,18 @@ class Artist
     @name = name
   end 
   
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
   def add_song(song)
     song.artist = self 
   end 
   
-  def songs 
-    Song.all.select {|song| song.artist == self}
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
   end 
   
 end 
