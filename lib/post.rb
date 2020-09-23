66762ef6e29c09f543dfbf49f3b96347a46c6e1a
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-23 06:30:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,6 +2,25 @@ class Post
   
   attr_accessor :title, :author
   
+   @@all = []
+   
+   def initialize(name)
+     @name = name 
+     save
+   end 
+   
+   def save
+     @@all << self
+   end 
+   
+   def self.all 
+     @@all
+   end 
+   
+   def artist_name
+     self.artist.name 
+   end 
+  
   def post_author(author_obj)
     @author << author_obj 
     author_obj.post = self 
